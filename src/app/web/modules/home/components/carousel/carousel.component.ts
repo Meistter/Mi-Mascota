3d884ee1623362463c3c14487eb122a039a5a576
Meistter <josueperez723@gmail.com>
2023-06-30 14:15:35 -0400

    Revert "carousel with bootstrap"
    This reverts commit 288d7b5683d10c3dc0048fd889a62c792aceee60.

--- src/app/web/modules/home/components/carousel/carousel.component.ts
+++ src/app/web/modules/home/components/carousel/carousel.component.ts
@@ -1,10 +1,4 @@
 import { Component } from '@angular/core';
-import Swiper, { Navigation, Pagination } from 'swiper';
-// import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
 
 @Component({
   selector: 'app-carousel',
@@ -13,8 +7,4 @@ import 'swiper/css/pagination';
 })
 export class CarouselComponent {
 
- images = [{url: '../../../../assets/banner_notext.jpg', indice: 0},
-           {url: '../../../../assets/bandera.jpg', indice: 1}]
-
-   
 }
