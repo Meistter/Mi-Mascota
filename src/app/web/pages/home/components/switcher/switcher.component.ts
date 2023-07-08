d68fe4958ce5602cd7a93f50eed8a5de0d791682
Josue Perez <josueperez723@gmail.com>
2023-07-07 20:21:47 -0400

    favicon changed, swicher buttons toggle, headernav transition

--- src/app/web/pages/home/components/switcher/switcher.component.ts
+++ src/app/web/pages/home/components/switcher/switcher.component.ts
@@ -9,4 +9,17 @@ export class SwitcherComponent {
 
   locations = [{value:'Barquisimeto'},{value: 'Valencia'},{value: 'Maracaibo'},{value: 'Caracas'}]
   selected = 'Barquisimeto';
+
+  btnIzquierda : boolean = false
+  btnDerecha : boolean = true
+
+  activarBtnIzquierda(){
+    this.btnIzquierda = true
+    this.btnDerecha = false
+  }
+  activarBtnDerecha(){
+    this.btnIzquierda = false
+    this.btnDerecha = true
+  }
+
 }
