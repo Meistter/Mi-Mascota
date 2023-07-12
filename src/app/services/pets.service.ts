e81d3758c60455ea655e9febbe60fc1b7375a340
Meistter <josueperez723@gmail.com>
2023-07-12 12:02:16 -0400

    add pet photos add

--- src/app/services/pets.service.ts
+++ src/app/services/pets.service.ts
@@ -12,7 +12,7 @@ export class PetsService {
   adoptionPets: Pet[] = [    
     {id:'0',name:'Arturo el Canguro', 
     description: 'Es un conejito muy bien portado, le encanta jugar comer zanahoria y dormir por las tardes',
-    age:2, health:'Buena', location:'Caracas', vaccinated:false, owner_name:'Josue Perez',castrated:false,disease:false,adoptionReason:'No tengo dinero para mantenerlo',remarks:'Es jugueton y muerde todo',
+    age:2, health:'Buena', location:'Caracas', vaccinated:false, owner_name:'Josue Perez',castrated:true,disease:false,adoptionReason:'No tengo dinero para mantenerlo',remarks:'Es jugueton y muerde todo',
     photos:[{url_mobile: 'https://i.postimg.cc/nhzfJByN/conejo.jpg', url_full:'', alt:'conejo-photo1'}]},
     {id:'1',name:'Ramon el Loro', 
     description: 'Un ave muy noble sabe cantar opera y a veces se destaca con un rap',
@@ -28,15 +28,15 @@ export class PetsService {
     photos:[{url_mobile: 'https://i.postimg.cc/3wvtxkL8/sofiareal-min.jpg', url_full:'', alt:''}]},
     {id:'4',name:'Max patitas cortas', 
     description: 'Es muy caprichoso y piqui para comer no come cualquier tipo de croqueta y le teme a la oscuridad, pero es bilingüe y es bonito',
-    age:1, health:'Buena', location:'Texas', vaccinated:true, owner_name:'Josue Perez',castrated:false,disease:false,adoptionReason:'No tengo dinero para mantenerlo',remarks:'Es jugueton y muerde todo',
-    photos:[{url_mobile: 'https://i.postimg.cc/TwjQnZMB/max-min.jpg', url_full:'', alt:''},{url_mobile: 'https://i.postimg.cc/3RFqhQD1/max2-min.jpg', url_full:'', alt:'max-photo2'},{url_mobile: 'https://i.postimg.cc/T124YNV6/max3-min.jpg', url_full:'', alt:'max-photo3'}]},
+    age:1, health:'Buena', location:'Texas', vaccinated:true, owner_name:'Josue Perez',castrated:true,disease:false,adoptionReason:'Hemos tenido dias dificiles y actualmente no tenemos dinero para mantenerlo y no queremos dejarlo en un refugio',remarks:'En el pasado Max ha tenido episodios ansiedad por el calor donde es necesario llevarlo a un lugar fresco y tener cuidado con el calor en sus patitas',
+    photos:[{url_mobile: 'https://i.postimg.cc/TwjQnZMB/max-min.jpg', url_full:'', alt:''},{url_mobile: 'https://i.postimg.cc/3RFqhQD1/max2-min.jpg', url_full:'', alt:'max-photo2'},{url_mobile: 'https://i.postimg.cc/T124YNV6/max3-min.jpg', url_full:'', alt:'max-photo3'},{url_mobile: 'https://i.postimg.cc/jqZPLHBv/max4.jpg', url_full:'', alt:'max-photo4'},{url_mobile: 'https://i.postimg.cc/cL8n9VC5/max5.jpg', url_full:'', alt:'max-photo5'}]},
     {id:'5',name:'Kira', 
     description: 'Me gusta jugar con los calcetines de mis padres y morderme la cola una y otra vez hasta que mis padres tienen que separarme porque me puedo hacer daño, solo como gatarina CatShow',
     age:3, health:'Buena', location:'Maracaibo', vaccinated:false, owner_name:'Josue Perez',castrated:false,disease:false,adoptionReason:'No tengo dinero para mantenerlo',remarks:'Es jugueton y muerde todo',
     photos:[{url_mobile: 'https://i.postimg.cc/ZRzVBJgr/sofia-min.jpg', url_full:'', alt:''}]},
     {id:'6',name:'Pirata Sparrow', 
     description: 'Amigable, le gustan los niños y es super cariñoso, buena compañía para personas mayores',
-    age:0.5, health:'Buena', location:'Texas', vaccinated:true, owner_name:'Josue Perez',castrated:false,disease:false,adoptionReason:'No tengo dinero para mantenerlo',remarks:'Es jugueton y muerde todo',
+    age:0.5, health:'Buena', location:'Texas', vaccinated:true, owner_name:'Josue Perez',castrated:false,disease:false,adoptionReason:'Hemos tenido dias dificiles y actualmente no tenemos dinero para mantenerlo y no queremos dejarlo en un refugio',remarks:'En el pasado Max ha tenido episodios ansiedad por el calor donde es necesario llevarlo a un lugar fresco y tener cuidado con el calor en sus patitas',
     photos:[{url_mobile: 'https://i.postimg.cc/sxVmmw3x/pirata-min.jpg', url_full:'', alt:''}]}
   ]
   rescuePets: Pet[] = []
