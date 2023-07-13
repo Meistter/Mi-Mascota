bb759324c5c7430ffd3bd94bafc17e5e64ab96be
Meistter <josueperez723@gmail.com>
2023-07-13 11:09:59 -0400

    Fix switcher select style, Add login and register routes on header and footer

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [  
-  
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
    {
     path: 'login',  
     loadChildren: () => import('./login/login.module').then((m) => m.LoginModule),
