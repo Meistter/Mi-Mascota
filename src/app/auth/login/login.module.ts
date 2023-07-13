2ccd609a2e54c64ff0fc2b6997f7aadca4cfca7e
Meistter <josueperez723@gmail.com>
2023-07-13 15:30:05 -0400

    Add login funtionality

--- src/app/auth/login/login.module.ts
+++ src/app/auth/login/login.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
+import { MaterialModule } from 'src/app/material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +13,9 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class LoginModule { }
--- src/app/auth/login/login/login.component.ts
+++ src/app/auth/login/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  hide : boolean = true
+  loginError : boolean = false //lo usaremos cuando las credenciales no sean correctas
+  constructor(private router: Router){}
+
+  loginForm = new FormGroup({
+    email : new FormControl('',[Validators.required, Validators.email]), 
+    password : new FormControl('',[Validators.required, Validators.minLength(5)]),
+    })
+
+    getRequiredMsg(){
+      return 'Este campo es Requerido'
+    }
+    getEmailMsg(){
+      return 'Por favor, Ingrese un email válido'
+    }
+
+    getLoginError(){
+      return 'Usuario o clave incorrectos'
+    }
+
+    login(event: Event){
+      event.preventDefault();
+      if(this.loginForm.valid){
+        console.log('logueado');
+        this.router.navigate(['/admin'])
+      }else{}      
+      
+    }
 }
