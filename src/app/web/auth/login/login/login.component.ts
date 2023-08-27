946bb8054e218e33de6cc6f9ab2ece4ff6de8d1b
meistter <josueperez723@gmail.com>
2023-08-27 12:17:47 -0400

    login error message

--- src/app/web/auth/login/login/login.component.ts
+++ src/app/web/auth/login/login/login.component.ts
@@ -1,32 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, ReplaySubject } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import { map } from 'rxjs/operators';
+import { Slide } from 'src/app/models/slide';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
 
+  // loginResponse$ = new Observable<string>()
+  loginResponse$ = new BehaviorSubject<any>(1)
   hide: boolean = true
-  message : boolean = false
+  message: boolean = false
   loginError: boolean = false //lo usaremos cuando las credenciales no sean correctas
-  redirectRoute : string | null = ''
-  constructor(private router: Router, private authService: AuthService, private route:ActivatedRoute) { }
-  
+  redirectRoute: string | null = ''
+  valor : string | null = null
+  constructor(private router: Router, private authService: AuthService, private route: ActivatedRoute) { }
 
- 
+
+
   ngOnInit(): void {
     this.route.queryParamMap.subscribe(params => {
-      if(params.get('query')){
+      if (params.get('query')) {
         this.message = true
-      }    
-      this.redirectRoute = params.get('route')    
-    })   
+      }
+      this.redirectRoute = params.get('route')
+    })
   }
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
@@ -50,21 +54,18 @@ export class LoginComponent implements OnInit{
       const value = this.loginForm.value
       const valueEmail = value.email
       const valuePass = value.password
-      if (valueEmail && valuePass) 
+      if (valueEmail && valuePass) {
       this.authService.login(valueEmail, valuePass) // ! esto es lo que responde el error de logueo
-          .then((rsp) => {                      
-            if(this.redirectRoute){
-              this.router.navigate(['/',this.redirectRoute]);
-            }else{
-              this.router.navigate(['/home']);
-            }            
-          })
-         
-          
-   
-      
+          .then((rsp) => {
             
-          
-    }
+            if (this.redirectRoute) {
+              this.router.navigate(['/', this.redirectRoute]);
+            } else {
+              this.router.navigate(['/home']);
+            }
+          })  
+          .catch(rsp =>{this.valor = 'Usuario o contraseña Incorrectos'})       
+      }
+    }    
   }
 }
