8a3aa40874d1d430a06a048f5c74c2e2cfa1f51f
meistter <josueperez723@gmail.com>
2023-08-27 12:29:04 -0400

    register required error fixed

--- src/app/models/slide.ts
+++ src/app/models/slide.ts
@@ -3,14 +3,3 @@ export interface Slide {
     url_full: string,
     alt: string
 }
-
-export interface Response {
-    __zone_symbol__state : number,
-    __zone_symbol__value : __zone_symbol__value
-
-}
-export interface  __zone_symbol__value {
-    code : string,
-    name : string,
-    message: string
-}
--- src/app/web/auth/register/register/register.component.ts
+++ src/app/web/auth/register/register/register.component.ts
@@ -8,60 +8,62 @@ import { SucessPopupComponent } from '../sucess-popup/sucess-popup.component';
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
- 
+
 })
 export class RegisterComponent {
 
-  hide : boolean = true
-  registerError : boolean = false //lo usaremos cuando las credenciales no sean correctas
-  termsRequired: boolean = true
-  responsabilityRequired: boolean = true
-  toggleTerms(event: any){if(!event.checked){this.termsRequired = true}else{this.termsRequired = false}}
-  toggleResponsability(event: any){if(!event.checked){this.responsabilityRequired = true}else{this.responsabilityRequired = false}}
-  
+  hide: boolean = true
+  registerError: boolean = false //lo usaremos cuando las credenciales no sean correctas
+  termsRequired: boolean = false
+  responsabilityRequired: boolean = false
 
-  constructor(private router: Router, private matDialog: MatDialog, private authService: AuthService){}
+  constructor(private router: Router, private matDialog: MatDialog, private authService: AuthService) { }
 
   registerForm = new FormGroup({
-    name: new FormControl('',[Validators.required]),
-    lastname: new FormControl('',[Validators.required]),
-    email : new FormControl('',[Validators.required, Validators.email]), 
-    password : new FormControl('',[Validators.required, Validators.minLength(6)]),
-    date: new FormControl('',[Validators.required]),
-    terms: new FormControl(false,[Validators.required]),
-    responsability: new FormControl(false,[Validators.required]),
-    })
+    name: new FormControl('', [Validators.required]),
+    lastname: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    date: new FormControl('', [Validators.required]),
+    terms: new FormControl(false, [Validators.required]),
+    responsability: new FormControl(false, [Validators.required]),
+  })
 
-    getRequiredMsg(){
-      return 'Este campo es Requerido'
-    }
-    getEmailMsg(){
-      return 'Por favor, Ingrese un email válido'
-    }
+  getRequiredMsg() {
+    return 'Este campo es Requerido'
+  }
+  getEmailMsg() {
+    return 'Por favor, Ingrese un email válido'
+  }
 
-    getRegisterError(){
-      return 'Error al realizar el registro, por favor intenta más tarde'
-    }
-    openDialog(){
-      this.matDialog.open(SucessPopupComponent)
-    }
-    register(){
-      if(this.registerForm.valid){
-        const value = this.registerForm.value
-        const valueEmail = value.email
-        const valuePass = value.password
-        if (valueEmail && valuePass)
+  getRegisterError() {
+    return 'Error al realizar el registro, por favor intenta más tarde'
+  }
+  openDialog() {
+    this.matDialog.open(SucessPopupComponent)
+  }
+  toggleTerms() {
+    this.termsRequired = !this.termsRequired
+  }
+  toggleResponsability() {
+    this.responsabilityRequired = !this.responsabilityRequired
+  }
+  register() {
+    if (this.registerForm.valid && this.termsRequired && this.responsabilityRequired) {
+      const value = this.registerForm.value
+      const valueEmail = value.email
+      const valuePass = value.password
+      if (valueEmail && valuePass)
         this.authService.createUser(valueEmail, valuePass)
-        .then((rsp) => {        
+          .then((rsp) => {
             this.openDialog()
             this.router.navigate(['/home']);
-          
-        });
-      }
-      
-         
-      
+          });
     }
 
-    
+
+
+  }
+
+
 }
